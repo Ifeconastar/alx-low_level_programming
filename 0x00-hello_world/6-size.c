0b7481ab87a5d4d704b039b02aaf41bcbbaec8f0
Izuegbu Ifeanyichukwu Joseph <ifeanyiizuegbu29@gmail.com>
2022-09-09 09:40:19 -0700

    size 6again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * 
+ * main - Entry point 
  * Description - prints"programming is like building a multilingual puzzle"
- * 
  * Return : Always 0 (Success)
  */
-
 
 int main(void)
 {
