e87387f74f7884507567f9defe733d23a8f62347
Izuegbu Ifeanyichukwu Joseph <ifeanyiizuegbu29@gmail.com>
2022-09-09 11:04:24 -0700

    done size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description - prints"programming is like building a multilingual puzzle"
- * Return : Always 0 (Success)
+ * * main - void
+ * * Description - prints"the size of various types on the computer"
+ * * Return - Always 0 (Success)
  */
-
-int main(void)
+int(void)
 {
 printf("Size of a char: %d byte(s)\n", sizeof(char));
-
 printf("Size of an int: %d byte(s)\n", sizeof(int));
-
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-
 printf("Size of a float: %d byte(s)\n", sizeof(float));
-
 return (0);
 }
+
