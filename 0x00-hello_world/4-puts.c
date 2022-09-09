c9f07c8f95f6ab97f88be769822b8b26ff51b61e
Izuegbu Ifeanyichukwu Joseph <ifeanyiizuegbu29@gmail.com>
2022-09-08 18:58:39 -0700

    4 put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  *
  * * main - print the string in the put function
@@ -13,17 +11,15 @@
  * * this program prints "Programming is like building a multilingual puzzle
  *
  * * Return: 0
- *
- *
-  */
+ */  
 
 int main(void)
 
 {
 
-		puts("Programming is like building a multilingual puzzle\n");
+          puts("\"Programming is like building a multilingual puzzle\n");
 		                                                          
 
-			return (0);
+	  return (0);
 
 }
