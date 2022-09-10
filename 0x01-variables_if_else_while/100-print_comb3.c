a8ce9c5c01755141e627d42dd41e8bd3bb0cf18f
Izuegbu Ifeanyichukwu Joseph <ifeanyiizuegbu29@gmail.com>
2022-09-10 03:45:26 -0700

    100print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,25 +10,26 @@
  */
 int main(void)
 {
-int i, j;
-i = 48;
-j = 48;
-while (i < 58)
+int digit1 = 0, digit2;
+while (digit1 <= 9)
 {
-j = 48;
-while (j < 58)
+digit2 = 0;
+while (digit2 <= 9)
 {
-putchar(i);
-putchar(j);
-if (i < 57 || j < 57)
+if (digit1 != digit2 && digit1 < digit2)
 {
-putchar(44);
-putchar(32);
+putchar(digit1 + 48);
+putchar(digit2 + 48);
+if (digit1 + digit2 != 17)
+{
+putchar(',');
+putchar(' ');
 }
-j++;
 }
-i++;
+++digit2;
 }
-putchar(10);
+++digit1;
+}
+putchar('\n');
 return (0);
 }
