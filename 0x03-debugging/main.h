689abdb541753b42fa86319f64613c45cf86b238
Izuegbu Ifeanyichukwu Joseph <ifeanyiizuegbu29@gmail.com>
2022-09-16 11:45:31 -0700

    3 print update

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,8 @@
 
 void positive_or_negative(int i);
 int largest_number(int, int, int);
-void print_remaining_days(int, int, int);
+void print_remaining_days(int month, int day, int year);
+int main(void);
 int convert_day(int, int)
 
 #endif /* MAIN_H */
