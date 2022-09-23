e4ca3076f685b74e3843236659bc3a8e0ef82b11
Izuegbu Ifeanyichukwu Joseph <ifeanyiizuegbu29@gmail.com>
2022-09-22 22:14:23 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,10 +9,24 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int index = 0, dest_len = 0;
-while (dest[index++])
-dest_len++;
-for (index = 0; src[index] && index < n; index++
-dest[dest_len++] = src[index];
+int l1, l2, len;
+
+l1 = l2 = 0;
+while (dest[l1] != '\0')
+{
+l1++;
+}
+while (src[l2] != '\0')
+{
+l2++;
+}
+len = l2;
+l2 = 0;
+while (l2 < len && l2 < n)
+{
+dest[l1] = src[l2];
+l1++;
+l2++;
+}
 return (dest);
 }
