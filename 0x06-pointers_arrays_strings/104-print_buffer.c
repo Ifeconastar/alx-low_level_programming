a1a85b895379e81f48b9010e97773c81372d56e6
Izuegbu Ifeanyichukwu Joseph <ifeanyiizuegbu29@gmail.com>
2022-09-23 20:09:04 -0700

    104-print_buffer.c fix hot

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,19 +12,19 @@ void print_buffer(char *b, int size)
 	int byte, index;
 
 	for (byte = 0; byte < size; byte += 10)
-	{
-		printf("%08x: ", byte);
-	        for (index = 0; index < 10; index++)
 		{
+		printf("%08x: ", byte);
+		for (index = 0; index < 10; index++)
+			{
 			if ((index + byte) >= size)
 				printf("  ");
 			else
 				printf("%02x", *(b + index + byte));
 			if ((index % 2) != 0 && index != 0)
 				printf(" ");
-		}
+			}
 		for (index = 0; index < 10; index++)
-		{
+			{
 			if ((index + byte) >= size)
 				break;
 			else if (*(b + index + byte) >= 31 &&
@@ -32,11 +32,11 @@ void print_buffer(char *b, int size)
 				printf("%c", *(b + index + byte));
 			else
 				printf(".");
-		}
+			}
 		if (byte >= size)
 			continue;
 		printf("\n");
-	}
+		}
 	if (size <= 0)
 		printf("\n");
 }
