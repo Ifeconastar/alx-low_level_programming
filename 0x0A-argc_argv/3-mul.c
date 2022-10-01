d960eb4f3aa902bf261a3bbb4f4cabf3492cf916
Izuegbu Ifeanyichukwu Joseph <ifeanyiizuegbu29@gmail.com>
2022-09-30 17:54:08 -0700

    0- 4 args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: The arguement counter
+ * @argv: The arguement values
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv[])
+{
+(void) argc;
+
+if (argc < 3)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
+
