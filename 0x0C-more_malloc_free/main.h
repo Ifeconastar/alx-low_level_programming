ea40bd257da8dd9f5811d53229b0b6faa30eb024
Izuegbu Ifeanyichukwu Joseph <ifeanyiizuegbu29@gmail.com>
2022-10-07 18:14:21 -0700

    update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,8 @@
 
 void *malloc_checked(unsigned int b);
 char *_memset(char *s, char b, unsigned int n);
+int get_digit(char c);
+int main(int argc, char *argv[]);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
