93d7d49bf8941bfc78231482f1c24f4e298c71fe
Izuegbu Ifeanyichukwu Joseph <ifeanyiizuegbu29@gmail.com>
2022-10-12 22:38:11 -0700

    print name update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
  * print_name - function that prints a name
  * @name: name to be printed
- * @f: function passed in
+ * @f: pointer to a function that prints a name
  *
  * Return: Nothing
  */
@@ -12,5 +12,6 @@ void print_name(char *name, void (*f)(char *))
 {
 if (name == NULL || f == NULL)
 return;
+
 f(name);
 }
